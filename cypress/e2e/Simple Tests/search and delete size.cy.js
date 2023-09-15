cc94d94ec996f3d676b2175f4f3b7e13f5f01e3d
Rabik Thapa <rabikthapa00@gmail.com>
2023-09-15 14:09:20 +0545

    Filter and Delete Size option added

---
+++ cypress/e2e/Simple Tests/search and delete size.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+//delete top inventory every time
+
+it('CATEGORIES SEARCH', function(){
+    //LOGIN
+    cy.visit('http://kidoclo.mrturingdev.com//'); 
+    cy.get('#email').type('rabikthapa00@gmail.com');
+    cy.get('#password').type('P@ssw0rd5789');
+    cy.get('.btn').click();
+
+// click on menu
+    cy.get('.nav-item > .bx').click();
+
+    //click on size
+    cy.get(':nth-child(7) > .menu-link').click();
+
+    //search for size to be deleted
+    cy.get('#DataTables_Table_0_length > :nth-child(1) > .form-control').type('sano');
+    
+ 
+    //click on filter
+    cy.get(':nth-child(2) > .dt-button').click();
+
+    //click on delete icon
+    cy.get('.delete-record').click();
+
+    //click on confirm delete
+    cy.get('.btn-danger').click();
+
+
+
+
+    });

205db4a53f779f390511596bccdb21ddf98d6872
Rabik Thapa <rabikthapa00@gmail.com>
2023-09-21 14:48:36 +0545

    Mochawesome added with reports

--- cypress.config.js
+++ cypress.config.js
@@ -8,3 +8,12 @@ module.exports = {
 };
 
 
+const { defineConfig } = require('cypress');
+module.exports = defineConfig({
+reporter: 'cypress-mochawesome-reporter',
+e2e: {
+setupNodeEvents(on, config) {
+require('cypress-mochawesome-reporter/plugin')(on);
+},
+},
+});
